12c7b7aad87f0ef5eeae0ba5296c65fee187a9d7
Tikhon <loki_deleoni@live.com>
2023-06-15 20:57:24 +0300

    add logger to HW_1 / Task_3

--- HW_1/HW_tsk_03.java
+++ HW_1/HW_tsk_03.java
@@ -1,9 +1,24 @@
 package HomeWork.HW_1;
 
+import HomeWork.HW_2.Task_2.Task_2;
+
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class HW_tsk_03 {
-    public static void main(String [] args) {
+    public static void main(String [] args) throws IOException {
+
+        Logger logger = Logger.getLogger(HW_tsk_03.class.getName());
+        logger.setLevel(Level.INFO);
+        FileHandler data = new FileHandler("C:\\Users\\loki_\\OneDrive\\Документы\\Учеба\\Разработчик\\8.ОПП (на JAVA)\\HomeWork\\HW_1\\calc_log.txt");
+        logger.addHandler(data);
+        SimpleFormatter sFormat = new SimpleFormatter();
+        data.setFormatter(sFormat);
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Please, enter 1th number: ");
@@ -18,22 +33,30 @@ public class HW_tsk_03 {
         switch (operator) {
             case ("+"):
                 System.out.printf("%f + %f = %f", num_1, num_2, num_1 + num_2);
+                System.out.println();
+                logger.info("USER activity: " + num_1 + " + " + num_2 + " = " + (num_1 + num_2));
                 break;
 
             case ("-"):
                 System.out.printf("%f - %f = %f", num_1, num_2, num_1 - num_2);
+                System.out.println();
+                logger.info("USER activity: " + num_1 + " - " + num_2 + " = " + (num_1 - num_2));
                 break;
 
             case ("*"):
                 System.out.printf("%f x %f = %f", num_1, num_2, num_1 * num_2);
+                System.out.println();
+                logger.info("USER activity: " + num_1 + " x " + num_2 + " = " + (num_1 * num_2));
                 break;
 
             case ("/"):
                 System.out.printf("%f / %f = %f", num_1, num_2, num_1 / num_2);
+                System.out.println();
+                logger.info("USER activity: " + num_1 + " / " + num_2 + " = " + (num_1 / num_2));
                 break;
             default:
                 System.out.println("Something goes wrong...bye!");
-
+                logger.info("USER activity: " + "input_1 = " + num_1 + "input_2 = " + num_2);
         }
     }
 }
