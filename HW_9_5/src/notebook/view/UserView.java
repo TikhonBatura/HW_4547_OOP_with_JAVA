8023dec0d6bbad4bf7ae85ea8f19def86692a088
Tikhon <loki_deleoni@live.com>
2023-07-16 15:49:51 +0300

    re-writte code from lesson 5.

---
+++ HW_9_5/src/notebook/view/UserView.java
@@ -0,0 +1,72 @@
+package notebook.view;
+
+import notebook.controller.UserController;
+import notebook.model.User;
+import notebook.util.Commands;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class UserView {
+    private final UserController userController;
+
+    public UserView(UserController userController) {
+        this.userController = userController;
+    }
+
+    public void run(){
+        Commands com;
+
+        while (true) {
+            String command = prompt("Введите команду: ");
+            com = Commands.valueOf(command);
+            if (com == Commands.EXIT) return;
+            switch (com) {
+                case CREATE:
+                    String firstName = prompt("Имя: ");
+                    String lastName = prompt("Фамилия: ");
+                    String phone = prompt("Номер телефона: ");
+                    userController.saveUser(new User(firstName, lastName, phone));
+                    break;
+                case READ:
+                    String id = prompt("Идентификатор пользователя: ");
+                    try {
+                        User user = userController.readUser(Long.parseLong(id));
+                        System.out.println(user);
+                        System.out.println();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                case LIST:
+
+                    try {
+                        List<User> users = userController.readAllUsers();
+                        System.out.println(users);
+
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                case UPDATE:
+                    try {
+                        firstName = prompt("Имя: ");
+                        lastName = prompt("Фамилия: ");
+                        phone = prompt("Номер телефона: ");
+                        Long userID = Long.parseLong(prompt("Введите идентификатор пользователя: "));
+                        User updated = new User(firstName, lastName, phone);
+                        userController.updateUser(userID, updated);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+            }
+        }
+    }
+
+    private String prompt(String message) {
+        Scanner in = new Scanner(System.in);
+        System.out.print(message);
+        return in.nextLine();
+    }
+}
