563051655ecdef51c2ec603142e5072fce47c1b0
Tikhon <loki_deleoni@live.com>
2023-07-18 23:50:10 +0300

    Done 1th part of HW9_6

--- HW_9_6/src/homework/Main.java
+++ HW_9_6/src/homework/Main.java
@@ -1,9 +1,15 @@
 package homework;
 
+import homework.Reports.ReportPrint;
+import homework.Users.User;
+
 public class Main{
     public static void main(String[] args){
-        User user = new User("Bob");
+        User user = new User(1,"Bob");
         user.report();
         user.save();
+
+        ReportPrint print = new ReportPrint();
+        print.report();
     }
 }
